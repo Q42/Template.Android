0a989441eeb12a0fa766a8417567cfe453bc20d3
Frank1234 <frankf@q42.nl>
2023-02-13 18:07:37 +0100

    ADD Napier logging

--- app/src/main/kotlin/nl/q42/template/MainApplication.kt
+++ app/src/main/kotlin/nl/q42/template/MainApplication.kt
@@ -1,12 +1,19 @@
 package nl.q42.template
 
 import android.app.Application
+import com.ramcosta.composedestinations.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 
 @HiltAndroidApp
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Napier.base(DebugAntilog())
+        }
     }
 }
