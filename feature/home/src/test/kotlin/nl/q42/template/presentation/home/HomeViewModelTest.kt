15e092e5d7a81c23f87d033058b24389d9b968a0
Frank1234 <frankf@q42.nl>
2023-09-28 15:30:08 +0200

    BUGFIX unit test fix

--- feature/home/src/test/kotlin/nl/q42/template/presentation/home/HomeViewModelTest.kt
+++ feature/home/src/test/kotlin/nl/q42/template/presentation/home/HomeViewModelTest.kt
@@ -2,7 +2,6 @@ package nl.q42.template.presentation.home
 
 import app.cash.turbine.test
 import io.mockk.coEvery
-import io.mockk.every
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.delay
@@ -10,18 +9,19 @@ import kotlinx.coroutines.test.runTest
 import nl.q42.template.actionresult.domain.ActionResult
 import nl.q42.template.domain.user.model.User
 import nl.q42.template.domain.user.usecase.GetUserUseCase
+import nl.q42.template.ui.presentation.ViewStateString
 import org.junit.Rule
 import org.junit.Test
 import kotlin.time.Duration.Companion.seconds
 
-
-class HomeViewModelTest(){
+class HomeViewModelTest() {
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
+
     @Test
-    fun `WHEN I subscribe to uiState with a slow UserUseCase THEN I get the loading state and expected email address`() = runTest{
+    fun `WHEN I subscribe to uiState with a slow UserUseCase THEN I get the loading state and expected email address`() = runTest {
         val getUserUseCaseMock: GetUserUseCase = mockk()
-        coEvery {getUserUseCaseMock.invoke() }.coAnswers {
+        coEvery { getUserUseCaseMock.invoke() }.coAnswers {
             // demonstration of test scheduler. This does not actually block the test for 4 seconds
             delay(4.seconds)
             ActionResult.Success(User("test@test.com"))
@@ -30,7 +30,7 @@ class HomeViewModelTest(){
         val viewModel = HomeViewModel(getUserUseCaseMock, mockk())
 
         viewModel.uiState.test {
-            val expectedData: HomeViewState = HomeViewState.Data("test@test.com")
+            val expectedData: HomeViewState = HomeViewState.Data(ViewStateString.Basic("test@test.com"))
 
             assertEquals(HomeViewState.Loading, awaitItem())
             assertEquals(expectedData, awaitItem())
@@ -38,19 +38,19 @@ class HomeViewModelTest(){
     }
 
     @Test
-    fun `WHEN I subscribe to uiState with a fast UserUseCase THEN I get expected email address immediately`() = runTest{
+    fun `WHEN I subscribe to uiState with a fast UserUseCase THEN I get expected email address immediately`() = runTest {
         val getUserUseCaseMock: GetUserUseCase = mockk()
         coEvery { getUserUseCaseMock.invoke() }.returns(
-            ActionResult.Success(User("test@test.com")
-        ))
-
+            ActionResult.Success(
+                User("test@test.com")
+            )
+        )
 
         val viewModel = HomeViewModel(getUserUseCaseMock, mockk())
 
         viewModel.uiState.test {
-            val expectedData: HomeViewState = HomeViewState.Data("test@test.com")
+            val expectedData: HomeViewState = HomeViewState.Data(ViewStateString.Basic("test@test.com"))
             assertEquals(expectedData, awaitItem())
         }
     }
-
 }
