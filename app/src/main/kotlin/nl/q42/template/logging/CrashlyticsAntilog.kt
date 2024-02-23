f126cf17da12eefaeaa0b20932cd0d2e6fd7e6e5
Nino van Hooff <nino@q42.nl>
2024-02-23 16:57:56 +0100

    CHANGE don't send DEBUG or VERBOSE messages to logcat for release builds
    crash-logging

--- app/src/main/kotlin/nl/q42/template/logging/CrashlyticsAntilog.kt
+++ app/src/main/kotlin/nl/q42/template/logging/CrashlyticsAntilog.kt
@@ -5,10 +5,17 @@ import com.google.firebase.ktx.Firebase
 import io.github.aakira.napier.Antilog
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.LogLevel
+import nl.q42.template.BuildConfig
 
-/** A value suitable for Crashlytics */
+/** A value suitable for Crashlytics
+ * This value is used to truncate the user-defined message and the exception message
+ * In theory, the message sent to Crashlytics could therefore be 2x this value
+ */
 private const val MAX_CHARS_IN_LOG = 1200
 
+/** A Crashlytics logger. The name Antilog might be an unfortunate choice by the Napier library;
+ * this is not a stub
+ */
 class CrashlyticsAntilog : Antilog() {
 
     private val logcatAntilog = DebugAntilog()
@@ -19,11 +26,12 @@ class CrashlyticsAntilog : Antilog() {
         throwable: Throwable?,
         message: String?
     ) {
-        // also send to logcat
-        logcatAntilog.log(priority, tag, throwable, message)
+        if (BuildConfig.DEBUG || priority > LogLevel.DEBUG) {
+            // also send to logcat
+            logcatAntilog.log(priority, tag, throwable, message)
+        }
 
         val limitedMessage = message?.take(MAX_CHARS_IN_LOG) // to avoid OutOfMemoryError's
-
 
         if ((priority == LogLevel.INFO || priority == LogLevel.DEBUG || priority == LogLevel.WARNING) && limitedMessage != null) {
             val errorMessage = throwable?.let {
