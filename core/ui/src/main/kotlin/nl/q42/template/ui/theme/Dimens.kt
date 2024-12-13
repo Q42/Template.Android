157a9536180b808ff2b0c15cfce449531f63109b
Nino van Hooff <ninovanhooff@gmail.com>
2024-12-13 11:47:52 +0100

    ADD buttonSpacing comment

--- core/ui/src/main/kotlin/nl/q42/template/ui/theme/Dimens.kt
+++ core/ui/src/main/kotlin/nl/q42/template/ui/theme/Dimens.kt
@@ -4,5 +4,6 @@ import androidx.compose.ui.unit.dp
 
 object Dimens {
     val componentSpacingVertical = 24.dp
+    /** Spacing between 2 vertically stacked buttons */
     val buttonSpacingVertical = 8.dp
 }
