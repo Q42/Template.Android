731fa3afbe7e9988f020a55cf7f62749054a9920
Nino van Hooff <ninovanhooff@gmail.com>
2025-06-30 15:58:49 +0200

    FIX keyboard insets
    Template.Android-148-scaffold

--- feature/home/src/main/kotlin/nl/q42/template/home/second/ui/HomeSecondContent.kt
+++ feature/home/src/main/kotlin/nl/q42/template/home/second/ui/HomeSecondContent.kt
@@ -1,17 +1,22 @@
 package nl.q42.template.home.second.ui
 
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import nl.q42.template.home.second.presentation.HomeSecondViewState
 import nl.q42.template.ui.compose.composables.text.H1Text
 import nl.q42.template.ui.compose.composables.widgets.TemplateButton
 import nl.q42.template.ui.compose.composables.window.ColumnScreenContent
-import nl.q42.template.ui.theme.Dimens
 import nl.q42.template.ui.theme.PreviewAll
 import nl.q42.template.ui.theme.TemplateTheme
 
@@ -30,7 +35,23 @@ fun HomeSecondContent(
 
         H1Text(text = viewState.title)
 
-        Spacer(Modifier.height(Dimens.componentSpacingVertical))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(600.dp)
+                .background(Color.Gray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("A very high box to test scrolling. Tapping the TextField below show show the keyboard and scroll the content up." +
+                    "The AppBar should stay visible and the content should not be obscured by the keyboard.")
+        }
+
+        TextField(
+            value = "test text input",
+            onValueChange = { },
+            label = { Text("Text Field") },
+            modifier = Modifier.height(56.dp)
+        )
 
         TemplateButton("Close", onClick = onBackClicked)
     }
@@ -39,7 +60,7 @@ fun HomeSecondContent(
 @Composable
 @PreviewAll
 private fun HomeSecondContentPreview() {
-    TemplateTheme{
+    TemplateTheme {
         HomeSecondContent(
             viewState = HomeSecondViewState(title = "Home Second Screen"),
             onBackClicked = {},
