c948fd7f4fd1a7adfb65f3b23c8e90b675db9834
Nino van Hooff <ninovanhooff@gmail.com>
2025-07-25 14:01:43 +0200

    FIX merge conflicts
    TEMPLATE-154/revert-template-prefix-to-app

--- core/ui/src/main/kotlin/nl/q42/template/ui/theme/AppTheme.kt
+++ core/ui/src/main/kotlin/nl/q42/template/ui/theme/AppTheme.kt
@@ -2,19 +2,26 @@ package nl.q42.template.ui.theme
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Modifier
 import nl.q42.template.ui.compose.composables.widgets.AppSurface
 
 private val LocalAppTypography = staticCompositionLocalOf { AppTypography() }
 private val LocalAppColorScheme = staticCompositionLocalOf<AppColorScheme> {
+    // Dummy default, will be replaced for the actual tokens by the Provider
     AppColorSchemeLight
 }
 private val LocalAppShapes = staticCompositionLocalOf { AppShapes() }
@@ -27,16 +34,25 @@ fun AppTheme(
     shapes: AppShapes = AppTheme.shapes,
     content: @Composable () -> Unit
 ) {
-    CompositionLocalProvider(
-        LocalAppTypography provides typography,
-        LocalAppColorScheme provides if (darkTheme) AppColorSchemeDark else AppColorSchemeLight,
-        LocalAppShapes provides shapes,
-        LocalRippleConfiguration provides AppRippleConfiguration,
-        LocalIndication provides AppRipple,
-        LocalTextStyle provides LocalTextStyle.current.merge(typography.body),
-        LocalContentColor provides colors.textPrimary,
-        content = content
-    )
+
+    MaterialTheme(
+        // materialtheme provides some defaults for eg. tonal elevation of Dialogs
+        colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme(),
+    ) {
+        CompositionLocalProvider(
+            LocalAppTypography provides typography,
+            LocalAppColorScheme provides if (darkTheme) AppColorSchemeDark else AppColorSchemeLight,
+            LocalAppShapes provides shapes,
+            /** configures the ripple for material components */
+            LocalRippleConfiguration provides AppRippleConfiguration,
+            /** needed for non-material components to have a material ripple. eg [Modifier.clickable] */
+            LocalIndication provides AppRipple,
+            /** merges the platform style with our type, @see [ProvideTextStyle] for more context */
+            LocalTextStyle provides LocalTextStyle.current.merge(typography.body),
+            LocalContentColor provides colors.textPrimary,
+            content = content
+        )
+    }
 }
 
 object AppTheme {
