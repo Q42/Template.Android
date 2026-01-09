e05e8591f01f5d6b5a50f76b03648a22b5ea6c32
Nino van Hooff <ninovanhooff@gmail.com>
2026-01-09 16:54:50 +0100

    WIP failed attempt to pass navigation parameters
    feature/template-194-navigation3

--- app/src/main/kotlin/nl/q42/template/navigation/HomeEntry.kt
+++ app/src/main/kotlin/nl/q42/template/navigation/HomeEntry.kt
@@ -4,20 +4,22 @@ import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import nl.q42.template.home.main.presentation.HomeViewModel
 import nl.q42.template.home.main.ui.HomeScreen
+import nl.q42.template.home.second.presentation.HomeSecondViewModel
+import nl.q42.template.home.second.ui.HomeSecondScreen
 import nl.q42.template.navigation.viewmodel.InitNavigator
 import nl.q42.template.navigation.viewmodel.Navigator
 import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
 
 internal fun EntryProviderScope<NavKey>.homeEntry(navigator: Navigator) {
     entry<Destination.Home> {
         val viewModel: HomeViewModel = koinViewModel()
         InitNavigator(navigator = navigator, routeNavigator = viewModel)
-
         HomeScreen(viewModel = viewModel)
     }
-    entry<Destination.HomeSecond> {
-        val viewModel: HomeViewModel = koinViewModel()
+    entry<Destination.HomeSecond> { key ->
+        val viewModel: HomeSecondViewModel = koinViewModel(parameters = { parametersOf(key) } ) // todo this does not work
         InitNavigator(navigator = navigator, routeNavigator = viewModel)
-        HomeScreen(viewModel = viewModel)
+        HomeSecondScreen(viewModel = viewModel)
     }
 }
