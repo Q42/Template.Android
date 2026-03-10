50cf9aa8d890ee2811a742b633893401c0058f0a
Nino van Hooff <ninovanhooff@gmail.com>
2026-03-10 12:11:31 +0100

    FIX deeplink char parsing
    feature/template-194-navigation3

--- app/src/main/kotlin/nl/q42/template/navigation/deeplink/DeepLinkPattern.kt
+++ app/src/main/kotlin/nl/q42/template/navigation/deeplink/DeepLinkPattern.kt
@@ -119,7 +119,7 @@ private fun getTypeParser(kind: SerialKind): TypeParser {
         PrimitiveKind.INT -> String::toInt
         PrimitiveKind.BOOLEAN -> String::toBoolean
         PrimitiveKind.BYTE -> String::toByte
-        PrimitiveKind.CHAR -> { it.single() }
+        PrimitiveKind.CHAR -> String::single
         PrimitiveKind.DOUBLE -> String::toDouble
         PrimitiveKind.FLOAT -> String::toFloat
         PrimitiveKind.LONG -> String::toLong
