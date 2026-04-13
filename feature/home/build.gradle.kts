0e9558c49df1572f347fa81a7362c37fc9da9939
Martijn Brekhof <martijn.brekhof@q42.nl>
2026-04-13 16:51:05 +0200

    CHANGE feature module build.gradle files to Kotlin Script
    Converts build files for feature:home and feature:onboarding.
    
    refactor/build_scripts_to_kts

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.library")
+    alias(libs.plugins.compose.compiler)
+    id("module.feature-and-app")
+    id("dep.navigation")
+}
+
+android {
+    val moduleName = "home"
+
+    namespace = "nl.q42.template.feature.$moduleName"
+}
+
+dependencies {
+    implementation(project(":domain:main"))
+    implementation(project(":core:ui"))
+    implementation(project(":core:actionresult"))
+    implementation(project(":core:navigation"))
+}
